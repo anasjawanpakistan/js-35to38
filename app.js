50e61a50e2673981b20f17c24f79f3d751c7ccb4
anasjawanpakistan <anasm0213@gmail.com>
2023-10-05 15:20:02 +0500

    Update

---
+++ app.js
@@ -0,0 +1,55 @@
+// Q2
+
+// function greet(){
+//     var firstName = prompt("Enter first name");
+//     var lastName = prompt("Enter last name");
+//     var fullname = firstName + lastName;
+//     console.log("fullname");
+// }
+
+// function greet(firstName, lastName){
+//     var fullname = firstName + lastName;
+//     console.log("fullname");
+// }
+
+// greet(prompt("Enter first name"),prompt("Enter last name"));
+
+
+// Q3
+
+// var Number1 = +prompt("Enter number to add");
+// var Number2 = +prompt("Enter number to add");
+
+// function sum(Number1, Number2){
+//     return Number1 + Number2;
+// }
+// var result = sum(Number1, Number2);
+// console.log(result);
+
+
+// Q4
+
+// var num1 = +prompt("Enter Number");
+// var operate = prompt("Enter Operator");
+// var num2 = +prompt("Enter Number");
+
+// var result = calc(num1, operate ,num2);
+// console.log(result);
+
+// function calc(num1,operate,num2){
+//     if (operate === "+"){
+//         return num1 + num2
+//     }else if(operate === "-"){
+//         return num1 - num2
+//       }else if(operate === "*"){
+//         return num1 * num2
+//       }   else if(operate === "/"){
+//         return num1 / num2
+//       }
+// }
+
+// Q5
+
+function squares(){
+    
+}
